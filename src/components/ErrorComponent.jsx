e83250a617200c60ab71bcb1add22980eb49a719
enzocazenave <encaze24@hotmail.com>
2022-08-14 02:39:56 -0300

    Add code

---
+++ src/components/ErrorComponent.jsx
@@ -0,0 +1,13 @@
+export const ErrorComponent = ({ text }) => {
+    return (
+        <>
+            {
+                (text.length != '') && 
+                    <div className="error-container">
+                        <i className="fas fa-info-circle"></i>
+                        <p>{ text }</p>
+                    </div>
+            }
+        </>
+    )
+}
